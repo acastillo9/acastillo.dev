e64d46807a654bb9e8ab227352e7214087739699
Andrés Castillo <info@acastillo.dev>
2023-07-24 01:38:47 -0500

    feat: add profile component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+	import Profile from './Profile.svelte';
+</script>
+
 <svelte:head>
 	<title>Andrés Castillo - Front End Developer</title>
 </svelte:head>
+
+<Profile />
--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -1,5 +1,5 @@
 <nav>
-	<div class="container mx-auto px-1 py-4">
+	<div class="container mx-auto p-4">
 		<a class="brand font-monserrat text-soft-yellow text-5xl" href="/">^Castillo</a>
 	</div>
 </nav>
